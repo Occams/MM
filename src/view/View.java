d274aa9f6b680624b7726a5c6e80213b24f74c17
Huber Bastian <huber@huberlaptop.(none)>
2011-01-21 11:50:09 +0100

    Init

---
+++ src/view/View.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class View extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
